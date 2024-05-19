61d0308ce171bdfd07b94ecd64e0a74618461b4c
Gabriel Patrício <gabrieltp087@gmail.com>
2024-05-19 11:43:02 -0300

    Create .env

--- apps/api/src/http/routes/auth/authenticate-with-github.ts
+++ apps/api/src/http/routes/auth/authenticate-with-github.ts
@@ -1,3 +1,4 @@
+import { env } from '@saas/env'
 import type { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
@@ -31,14 +32,14 @@ export async function authenticateWithGithub(app: FastifyInstance) {
         'https://github.com/login/oauth/access_token',
       )
 
-      githubOAuthURL.searchParams.set('client_id', 'Ov23li8Wrj3HJXgrqE2x')
+      githubOAuthURL.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
       githubOAuthURL.searchParams.set(
         'client_secret',
-        '897162cda34af2c52a8135030a524b225dfc6c42',
+        env.GITHUB_OAUTH_CLIENT_SECRETE,
       )
       githubOAuthURL.searchParams.set(
         'redirect_uri',
-        'http://localhost:3000/api/auth/callback',
+        env.GITHUB_OAUTH_CLIENT_REDIRECT_URL,
       )
       githubOAuthURL.searchParams.set('code', code)
 
